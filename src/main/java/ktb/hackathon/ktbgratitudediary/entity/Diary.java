b3d885c0519a55d6ab1645162fb0ddfb8b247755
정진 <maiload77@gmail.com>
2024-09-05 14:53:49 +0900

    feat: Diary 엔티티 추가

--- src/main/java/ktb/hackathon/ktbgratitudediary/entity/Diary.java
+++ src/main/java/ktb/hackathon/ktbgratitudediary/entity/Diary.java
@@ -1,16 +1,56 @@
 package ktb.hackathon.ktbgratitudediary.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
+
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "user_id"),
+        @Index(columnList = "createdAt")
+})
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Diary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
     private String content;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "fk_diary_user_id"))
     private User user;
+
+    private String emotion;
+
+    private int happiness;
+
+    private int weather;
+
+    @Column(columnDefinition = "TEXT")
+    private String vectorImage;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Template template;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
